543917896afaef121040532b00c5a8c7167afc83
Brad Williams <bradwilliams@northwesternmutual.com>
2016-04-26 17:59:12 -0500

    added route to app

--- src/server/app.js
+++ src/server/app.js
@@ -2,9 +2,11 @@ var path = require('path');
 var express = require('express');
 var compression = require('compression');
 var favicon = require('serve-favicon');
+var health = require('./routes/health');
 
 var app = express();
 
+app.use('/health', health);
 app.use(compression());
 app.use(favicon(path.join(__dirname, '/../../static/favicon.ico')));
 app.use(express['static']('static'));
