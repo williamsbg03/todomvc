ab90c6653e656cd012bd1bab6ac17030a3ed4e1d
Brad Williams <bradwilliams@northwesternmutual.com>
2016-08-09 11:23:42 -0500

    Adding base url

--- src/server/app.js
+++ src/server/app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var compression = require('compression');
 var favicon = require('serve-favicon');
 var health = require('./routes/health');
-
+var baseUrl = '/todo'
 var app = express();
 
-app.use('/health', health);
-app.use(compression());
-app.use(favicon(path.join(__dirname, '/../../static/favicon.ico')));
-app.use(express['static']('static'));
+app.use(baseUrl + '/health', health);
+app.use(baseUrl, compression());
+app.use(baseUrl, favicon(path.join(__dirname, '/../../static/favicon.ico')));
+app.use(baseUrl, express['static']('static'));
 
 module.exports = app;
